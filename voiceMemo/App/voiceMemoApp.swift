9bfd1f8974b19f3e523a41a213c018343b1cf441
TLtlbo <js1436kt@gmail.com>
2024-01-21 00:01:34 +0900

    타이머 기능에서 타이머가 종료 되면 알림이 발생하는 기능 구현 UNUserNotificationCenter를 통해 알림에 대한 정보를 작성 및 Adapter로 장착 UIBackgroundTaskIdentifier를 통해 백그라운드에서 앱이 동작하여 알림이 발생할 수 있게 구현

--- voiceMemo/App/voiceMemoApp.swift
+++ voiceMemo/App/voiceMemoApp.swift
@@ -7,6 +7,9 @@ import SwiftUI
 
 @main
 struct voiceMemoApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
   var body: some Scene {
     WindowGroup {
       OnboardingView()
--- voiceMemo/Feature/Home/Timer/TimerViewModel.swift
+++ voiceMemo/Feature/Home/Timer/TimerViewModel.swift
@@ -4,6 +4,7 @@
 //
 
 import Foundation
+import UIKit
 
 class TimerViewModel : ObservableObject {
     @Published var isDisplaySetTimeView : Bool
@@ -11,18 +12,21 @@ class TimerViewModel : ObservableObject {
     @Published var timer : Timer?
     @Published var timeRemaining : Int
     @Published var isPaused : Bool
+    var notificationService : NotificationService
     
     init(isDisplaySetTimeView: Bool = true,
          time: Time = .init(hours: 0, minutes: 0, seconds: 0),
          timer: Timer? = nil,
          timeRemaining: Int = 0,
-         isPaused: Bool = false
+         isPaused: Bool = false,
+         notificationService : NotificationService = .init()
     ) {
         self.isDisplaySetTimeView = isDisplaySetTimeView
         self.time = time
         self.timer = timer
         self.timeRemaining = timeRemaining
         self.isPaused = isPaused
+        self.notificationService = notificationService
     }
     
     
@@ -56,11 +60,26 @@ private extension TimerViewModel {
     func startTimer() {
         guard timer == nil else {return}
         
+        var backgroundTaskID : UIBackgroundTaskIdentifier?
+        backgroundTaskID = UIApplication.shared.beginBackgroundTask {
+            if let task = backgroundTaskID {
+                UIApplication.shared.endBackgroundTask(task)
+                backgroundTaskID = .invalid
+            }
+        }
+        
+        
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
             if self.timeRemaining > 0 {
                 self.timeRemaining -= 1
             } else {
                 self.stopTimer()
+                self.notificationService.sendNotification()
+                
+                if let task = backgroundTaskID {
+                    UIApplication.shared.endBackgroundTask(task)
+                    backgroundTaskID = .invalid
+                }
             }
         }
     }
