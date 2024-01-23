bd26ba59ff3197e72b123d21c10b5f40c67a172e
TLtlbo <js1436kt@gmail.com>
2024-01-23 20:43:18 +0900

    음성 메모 시작 버튼에 에니메이션 기능 구현 isAnimation 상태 변수의 값에 따라 작아졌다 커졌다처럼 보이게 구현

--- voiceMemo/Feature/Home/VoiceRecorder/VoiceRecorderView.swift
+++ voiceMemo/Feature/Home/VoiceRecorder/VoiceRecorderView.swift
@@ -278,9 +278,13 @@ private struct ProgressBar : View {
 //MARK: 녹음 버튼 뷰
 private struct RecordBtnView : View {
     @ObservedObject private var voiceRecorderViewModel : VoiceRecorderViewModel
+    @State private var isAnimation : Bool
     
-    fileprivate init(voiceRecorderViewModel: VoiceRecorderViewModel) {
+    fileprivate init(voiceRecorderViewModel: VoiceRecorderViewModel,
+                     isAnimation : Bool = false
+    ) {
         self.voiceRecorderViewModel = voiceRecorderViewModel
+        self.isAnimation = isAnimation
     }
     
     fileprivate var body: some View {
@@ -295,6 +299,15 @@ private struct RecordBtnView : View {
                 } label: {
                     if voiceRecorderViewModel.isRecording {
                         Image("mic_recording")
+                            .scaleEffect(isAnimation ? 1.5 : 1)
+                            .onAppear {
+                                withAnimation(.spring().repeatForever()) {
+                                    isAnimation.toggle()
+                                }
+                            }
+                            .onDisappear {
+                                isAnimation = false
+                            }
                     } else {
                         Image("mic")
                     }
